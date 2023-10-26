5325d7c300d5713bffb06fc4c7aa90fadb7bb575
AliAbdelAall <3laych@gmail.com>
2023-10-26 13:14:16 +0300

    Q1 function

--- Cycle 48/Assignment_3/Assignment_3.py
+++ Cycle 48/Assignment_3/Assignment_3.py
@@ -1,6 +1,18 @@
 # ########### User Login ############
 
-def userlogin():
+def userLogin():
     print("login")
     username = input("Enter yor username: ")
     return f"Welcome, {username}"
+
+
+# ########### Question_1 ############
+
+def addMatrices(matrix_1, matrix_2):
+    res = []
+    for i in range(len(matrix_1)):
+        row = []
+        for j in range(len(matrix_1(i))):
+            row.append(matrix_1[i][j]+matrix_2[i][j])
+        res.append(row)
+    return res

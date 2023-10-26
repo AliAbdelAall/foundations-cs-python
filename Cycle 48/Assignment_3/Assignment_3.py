b4afd14c46f135e0bf0074ae4c7136cf7e925613
AliAbdelAall <3laych@gmail.com>
2023-10-26 14:03:06 +0300

    menu function added

--- Cycle 48/Assignment_3/Assignment_3.py
+++ Cycle 48/Assignment_3/Assignment_3.py
@@ -8,7 +8,7 @@ def userLogin():
 
 # ########### Question_1 ############
 
-# ###### Input #######:
+# ###### Input #######
 
 def lenInput():
     num = input("Enter the length of the Matrix: ")
@@ -46,6 +46,8 @@ def inputMatrix(n, m):
     return matrix
 
 
+# ###### Function 1 #######
+
 def addMatrices(matrix_len, sub_len):  #
     matrix_1 = inputMatrix(matrix_len, sub_len)
     matrix_2 = inputMatrix(matrix_len, sub_len)
@@ -56,3 +58,16 @@ def addMatrices(matrix_len, sub_len):  #
             row.append(matrix_1[i][j] + matrix_2[i][j])
         res.append(row)
     return res
+
+
+# ########### Menu ############
+
+def Menu():
+    print("""
+    1. Add Matrices
+    2. Check Rotation
+    3. Invert Dictionary
+    4. Convert Matrix to Dictionary
+    5. Check Palindrome
+    6. Search for an Element & Merge Sort
+    7. Exit""")

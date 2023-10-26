292d97a5e445b2c67f3051f85a183b92746666f7
AliAbdelAall <3laych@gmail.com>
2023-10-26 17:03:48 +0300

    mergeSort function updated

--- Cycle 48/Assignment_3/Assignment_3.py
+++ Cycle 48/Assignment_3/Assignment_3.py
@@ -196,11 +196,11 @@ def merge(left_half, right_half):
 
     while left_index < len(left_half) and right_index < len(right_half):
 
-        if left_half[left_index] > right_half[right_index]:
+        if left_half[left_index] < right_half[right_index]:
             merged.append(left_half[left_index])
             left_index += 1
 
-        elif left_half[left_index] < right_half[right_index]:
+        elif left_half[left_index] > right_half[right_index]:
             merged.append(right_half[right_index])
             right_index += 1
 
